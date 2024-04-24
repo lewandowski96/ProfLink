116b6e0311431046f1c6e0e767f38dbebed4bd4e
dininduk <it21247972@my.sliit.lk>
2024-04-24 21:14:01 +0530

    fix: update desc : backend server configuration  updated for file upload

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose"); 
@@ -16,7 +17,8 @@ const app = express();
 // first the request is received to middleware and then to the actual routes.
 
 // this will enable us to access the request body
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
